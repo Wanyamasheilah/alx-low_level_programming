be2476d6a8eaeaf6b9d937e1198063b2178423fc
Wanyamasheilah <sheilahwanyama.com>
2023-09-08 06:53:09 -0700

    5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -9,16 +9,17 @@
 
 int main(void)
 {
-	char letter;
+char letter;
 
-	for (letter = 'a'; letter <= 'z'; letter++)
+for (letter = 'a'; letter <= 'z'; letter++)
 
-	{
-	if (letter != 'e' && letter != 'q')
-	putchar(letter);
-	}
-
-	putchar('\n');
-
-	return (0);
+{	
+if (letter != 'e' && letter != 'q')
+putchar(letter);
 }
+
+putchar('\n');
+
+return (0);
+}
+
