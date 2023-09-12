ae4251dc25f3569994d2ddc6d3d456a9a5b396ef
Wanyamasheilah <sheilahwanyama.com>
2023-09-12 14:03:28 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,14 +1,22 @@
 #include "main.h"
 
 /**
-  * print_alphabet - function to print alphabets in lower case
-  * Return: 0
+ * main - function
+ * Return: 0
  */
 
-void print_alphabet(void)
+int main(void)
 {
-char i;
-for (i = 'a' ; i <= 'z' ; i++)
-_putchar(i);
-_putchar('\n')	
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+
 }
+
