31fb8626f0f30c13c5374faef1e97d8ac4093ca4
Wanyamasheilah <sheilahwanyama.com>
2023-09-14 03:36:45 -0700

    3-print_remaining_days.c

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -14,7 +14,7 @@ void print_remaining_days(int month, int day, int year)
 {
 if ((year % 4 == 0 && (year % 400 == 0) || year % 100 != 0))
 {
-if (month >= 3  && day >= 60)
+if (month >= 3  && day >= 90)
 {
 day++;
 }
@@ -35,4 +35,3 @@ printf("Remaining days: %d\n", 365 - day);
 }
 }
 }
-
