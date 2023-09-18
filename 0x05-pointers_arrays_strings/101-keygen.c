9ad90c93283cae5446729640d707266bd636d451
Wanyamasheilah <sheilahwanyama.com>
2023-09-18 13:56:04 -0700

    101-keygen

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
@@ -11,26 +10,27 @@
  */
 int main(void)
 {
-int pass[100];
-int i, sum, n;
+	int pass[100];
+	int i, sum, n;
 
-sum = 0;
+	sum = 0;
 
-srand(time(NULL));
+	srand(time(NULL));
 
-for (i = 0; i < 100; i++)
-{
-pass[i] = rand() % 78;
-sum += (pass[i] + '0');
-putchar(pass[i] + '0');
-if ((2772-sum)-'0' < 78)
-{
-n = 2772-sum-'0';
-sum += n;
-putchar(n + '0');
-break;
+	for (i = 0; i < 100; i++)
+	{
+	pass[i] = rand() % 78;
+	sum += (pass[i] + '0');
+	putchar(pass[i] + '0');
+	if ((2772 - sum) - '0' < 78)
+	{
+	n = 2772 - sum - '0';
+	sum += n;
+	putchar(n + '0');
+	break;
+	}
+	}
+
+	return (0);
 }
-}
 
-return (0);
-}
